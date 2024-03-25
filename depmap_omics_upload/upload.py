39cea201f9a1b9fd420bcd38639d2ee9f782d272
5im1z <5imonedec9@gmail.com>
2024-03-25 16:17:24 +0000

    NA datatype for everything except wes

--- depmap_omics_upload/upload.py
+++ depmap_omics_upload/upload.py
@@ -375,14 +375,14 @@ def makeProfileTable(prs, columns=config["profile_table_cols"]):
     pr_table = mytracker.add_model_cols_to_prtable(["ModelID"])
     seq_table = mytracker.read_seq_table()
     mc_table = mytracker.read_mc_table()
+    pr_table["Stranded"] = pr_table["MainSequencingID"].map(dict(zip(seq_table.index, seq_table.stranded)))
+    pr_table["Source"] = pr_table["ModelCondition"].map(dict(zip(mc_table.index, mc_table.Source)))
+    pr_table = pr_table.loc[prs, columns].rename(columns={"Baits": "WESKit", "actual_seq_technology": "Product"})
     pr_table = pr_table[pr_table["Datatype"].isin(["rna", "wgs", "wes", "SNParray"])]
     pr_table.loc[
         pr_table[pr_table["Datatype"].isin(["rna", "wgs", "SNParray"])].index.tolist(),
         "WESKit",
     ] = ""
-    pr_table["Stranded"] = pr_table["MainSequencingID"].map(dict(zip(seq_table.index, seq_table.stranded)))
-    pr_table["Source"] = pr_table["ModelCondition"].map(dict(zip(mc_table.index, mc_table.Source)))
-    pr_table = pr_table.loc[prs, columns].rename(columns={"Baits": "WESKit", "actual_seq_technology": "Product"})
 
     mytracker.close_gumbo_client()
     return pr_table
